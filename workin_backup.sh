fd6e43b1b72139aa05a9d10003879cc0f0b4a7a7
Oyelola Oluwafemi Emmanuel <36579635+wafemi999@users.noreply.github.com>
2024-12-26 17:09:48 +0100

    Update workin_backup.sh

--- workin_backup.sh
+++ workin_backup.sh
@@ -3,7 +3,7 @@
 # This checks if the number of arguments is correct
 # If the number of arguments is incorrect ( $# != 2), print error message and exit
 if [[ $# != 2 ]]; then
-  echo "backup.sh target_directory_name destination_directory_name"
+  echo " backup.sh target_directory_name destination_directory_name "
   exit
 fi
 
